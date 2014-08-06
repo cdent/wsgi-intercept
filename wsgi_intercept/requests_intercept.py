dd5eada53c011c2c0e16ea32fae0517b01e8da5b
Sasha Hart <s@sashahart.net>
2014-08-06 13:24:05 -0500

    lame strict handling on behalf of old urllib3 in Debian
    converted super() to old-style __init__ calls

--- wsgi_intercept/requests_intercept.py
+++ wsgi_intercept/requests_intercept.py
@@ -1,6 +1,6 @@
 """Intercept HTTP connections that use `requests <http://docs.python-requests.org/en/latest/>`_.
 """
-
+import sys
 from . import WSGI_HTTPConnection, WSGI_HTTPSConnection, wsgi_fake_socket
 from requests.packages.urllib3.connectionpool import (HTTPConnectionPool,
         HTTPSConnectionPool)
@@ -12,11 +12,19 @@ wsgi_fake_socket.settimeout = lambda self, timeout: None
 
 
 class HTTP_WSGIInterceptor(WSGI_HTTPConnection, HTTPConnection):
-    pass
+    def __init__(self, *args, **kwargs):
+        if 'strict' in kwargs and sys.version_info > (3, 0):
+            kwargs.pop('strict')
+        WSGI_HTTPConnection.__init__(self, *args, **kwargs)
+        HTTPConnection.__init__(self, *args, **kwargs)
 
 
 class HTTPS_WSGIInterceptor(WSGI_HTTPSConnection, HTTPSConnection):
-    pass
+    def __init__(self, *args, **kwargs):
+        if 'strict' in kwargs and sys.version_info > (3, 0):
+            kwargs.pop('strict')
+        WSGI_HTTPSConnection.__init__(self, *args, **kwargs)
+        HTTPSConnection.__init__(self, *args, **kwargs)
 
 
 def install():
