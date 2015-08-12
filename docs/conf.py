b18c1181b2fa09adc3e099b99f17bfb6a800ecbd
Chris Dent <chris.dent@gmail.com>
2015-08-12 12:04:15 +0100

    Update copyright date

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'wsgi_intercept'
-copyright = '2014, Titus Brown, Kumar McMillan, Chris Dent'
+copyright = '2015, Titus Brown, Kumar McMillan, Chris Dent, Sasha Hart'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
