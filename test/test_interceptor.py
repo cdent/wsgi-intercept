9d73597f041139429825dfc4a924560a0ef9c396
Chris Dent <cdent@anticdent.org>
2015-12-28 14:22:35 +0000

    Intial stab at context manager based intercepts
    See test/test_interceptor for the expect use pattern.
    
    The idea here is that instead of the overrides being visible to the
    caller the caller just uses a context manager that surrounds the tests
    and that context manager does the right thing.
    
    Those things, at the moment, are exploratory, in an effort to find the
    right idiom. The use of classes is intentional, despite icky, to make
    overrides and the creation of external subclasses easier.
    
    One thing (of probably several) that remains to be determined is the best
    idiom for interception when running a large suite of tests with the same
    intercept? What controls the context entry and exit?
    
    Nothing written in stone here, just an exploration. The goals? Two:
    
    * make the external interface simple while allowing lots of changing
      underneath (to bring wsgi-intercept into the future)
    * explore options for adding hooks and the like (see #28 for a
      related idea)

---
+++ test/test_interceptor.py
@@ -0,0 +1,97 @@
+"""Tests of using the context manager style.
+
+The context manager is based on the InterceptFixture used in gabbi.
+"""
+
+
+from uuid import uuid4
+
+import py.test
+import requests
+from httplib2 import Http, ServerNotFoundError
+
+from wsgi_intercept.interceptor import Interceptor, Httplib2Interceptor, RequestsInterceptor
+from .wsgi_app import simple_app
+
+def app():
+    return simple_app
+
+def test_interceptor_instance():
+    hostname = str(uuid4())
+    port = 9999
+    interceptor = Httplib2Interceptor(app=app, host=hostname, port=port)
+    assert isinstance(interceptor, Interceptor)
+    assert interceptor.app == app
+    assert interceptor.host == hostname
+    assert interceptor.port == port
+    assert interceptor.script_name == ''
+
+
+def test_httplib2_interceptor_host():
+    hostname = str(uuid4())
+    port = 9999
+    http = Http()
+    with Httplib2Interceptor(app=app, host=hostname, port=port):
+        url = 'http://%s:%s/' % (hostname, port)
+        response, content = http.request(url)
+        assert response.status == 200
+        assert 'WSGI intercept successful!' in content
+
+
+def test_httplib2_interceptor_url():
+    hostname = str(uuid4())
+    port = 9999
+    url = 'http://%s:%s/' % (hostname, port)
+    http = Http()
+    with Httplib2Interceptor(app=app, url=url):
+        response, content = http.request(url)
+        assert response.status == 200
+        assert 'WSGI intercept successful!' in content
+
+
+def test_httplib2():
+    hostname = str(uuid4())
+    port = 9999
+    url = 'http://%s:%s/' % (hostname, port)
+    http = Http()
+    with Httplib2Interceptor(app=app, url=url):
+        response, content = http.request(url)
+        assert response.status == 200
+        assert 'WSGI intercept successful!' in content
+
+    # outside the context manager the intercept does not work
+    with py.test.raises(ServerNotFoundError):
+        http.request(url)
+
+def test_requests_interceptor_host():
+    hostname = str(uuid4())
+    port = 9999
+    http = Http()
+    with RequestsInterceptor(app=app, host=hostname, port=port):
+        url = 'http://%s:%s/' % (hostname, port)
+        response = requests.get(url)
+        assert response.status_code == 200
+        assert 'WSGI intercept successful!' in response.text
+
+
+def test_requests_interceptor_url():
+    hostname = str(uuid4())
+    port = 9999
+    url = 'http://%s:%s/' % (hostname, port)
+    with RequestsInterceptor(app=app, url=url):
+        response = requests.get(url)
+        assert response.status_code == 200
+        assert 'WSGI intercept successful!' in response.text
+
+def test_requests_in_out():
+    hostname = str(uuid4())
+    port = 9999
+    url = 'http://%s:%s/' % (hostname, port)
+    with RequestsInterceptor(app=app, url=url):
+        response = requests.get(url)
+        assert response.status_code == 200
+        assert 'WSGI intercept successful!' in response.text
+
+    # outside the context manager the intercept does not work
+    with py.test.raises(requests.ConnectionError):
+        requests.get(url)
---
+++ wsgi_intercept/interceptor.py
@@ -0,0 +1,67 @@
+"""Context manager based interception."""
+
+from importlib import import_module
+
+from six.moves.urllib import parse as urlparse
+
+import wsgi_intercept
+
+
+class Interceptor(object):
+
+    def __init__(self, app, host=None, port=None, prefix=None, url=None):
+        assert app
+        assert (host and port) or (url)
+
+        self.app = app
+
+        if url:
+            self._init_from_url(url)
+        else:
+            self.host = host
+            self.port = int(port)
+            self.script_name = prefix or ''
+
+        self._module = import_module('.%s' % self.MODULE_NAME,
+                                     package='wsgi_intercept')
+
+    def __enter__(self):
+        self._install_intercept()
+        wsgi_intercept.add_wsgi_intercept(self.host, self.port, self.app,
+                                          script_name=self.script_name)
+
+    def __exit__(self, exc_type, value, traceback):
+        wsgi_intercept.remove_wsgi_intercept(self.host, self.port)
+        self._uninstall_intercept()
+
+    def _init_from_url(self, url):
+        parsed_url = urlparse.urlsplit(url)
+        host, port = parsed_url.netloc.split(':')
+        if not port:
+            if parsed_url.scheme == 'https':
+                port = 443
+            else:
+                port = 80
+        path = parsed_url.path
+        if path == '/' or not path:
+            self.script_name = ''
+        else:
+            self.script_name = path
+        self.host = host
+        self.port = int(port)
+
+    def _install_intercept(self):
+        self._module.install()
+
+    def _uninstall_intercept(self):
+        self._module.uninstall()
+
+
+class Httplib2Interceptor(Interceptor):
+
+    MODULE_NAME = 'httplib2_intercept'
+
+
+class RequestsInterceptor(Interceptor):
+
+    MODULE_NAME = 'requests_intercept'
